d5a6525992e51e485c236e9048e2011333beb56d
Aeriff <aeriff@proton.me>
2025-11-24 00:56:18 +0800

    Updated the sign up and cancel registration logic, updated some configuration files

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Activity extends Model
 {
