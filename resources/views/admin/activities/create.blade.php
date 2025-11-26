2d3c9ad79f6367d6d72ce4a3a208dd36b2143555
Marleneeeeee <150803307+Marleneeeeee@users.noreply.github.com>
2025-11-26 10:51:24 +0800

    Enhance the frontend interface

--- resources/views/admin/activities/create.blade.php
+++ resources/views/admin/activities/create.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 
-@section('title', '编辑活动')
+@section('title', '发布新活动')
 
 @section('content')
     <h1 class="fw-bold custom-title mb-4">
-        <i class="bi bi-pencil-square me-2"></i> 编辑活动: {{ $activity->title }}
+        <i class="bi bi-megaphone me-2"></i> 发布新活动
     </h1>
     
     <div class="card shadow-lg rounded-4 border-0">
         <div class="card-body p-4 p-md-5">
-            <form action="{{ route('admin.activities.update', $activity->id) }}" method="POST">
-                @method('PUT')
-                @include('admin.activities._form', ['activity' => $activity, 'submitButtonText' => '更新活动'])
+            <form action="{{ route('admin.activities.store') }}" method="POST">
+                @include('admin.activities._form', ['submitButtonText' => '发布活动'])
             </form>
         </div>
     </div>
