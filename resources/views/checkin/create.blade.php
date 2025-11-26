f3732dcc1f0e0b03b409905107e662d7cf4a865a
Marleneeeeee <150803307+Marleneeeeee@users.noreply.github.com>
2025-11-26 11:09:06 +0800

    Enhance the frontend interface

--- resources/views/checkin/create.blade.php
+++ resources/views/checkin/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', '活动签到')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card shadow-sm">
@@ -53,4 +53,5 @@
             </div>
         </div>
     </div>
+
 @endsection
